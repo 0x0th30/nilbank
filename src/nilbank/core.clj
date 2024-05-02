981411ba2b0cec49e81c818704ae753ccd6503b9
0x0th30 <wrk.0x0th30@proton.me>
2024-05-02 05:43:55 -0300

    chore: creates base structure

---
+++ src/nilbank/core.clj
@@ -0,0 +1,7 @@
+(ns nilbank.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/nilbank/core_test.clj
@@ -0,0 +1,6 @@
+(ns nilbank.core-test
+  (:require [clojure.test :refer [deftest is testing]]))
+
+(deftest dumb-test
+  (testing "dumb testing"
+    (is (= 1 1))))
